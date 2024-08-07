7a7835436ccb616b8d2c408a2d063835ffb5280a
Steven Giesel <stgiesel35@gmail.com>
2024-08-07 20:25:23 +0200

    Simplify Hub

--- Backend/ExpenseHub.cs
+++ Backend/ExpenseHub.cs
@@ -26,10 +26,4 @@ namespace ExpenseTracker;
    |<-------------------------|                            |
    |                          |                            |
  */
-public class ExpenseHub : Hub
-{
-    public async Task NotifyBudgetExceeded(string user, string message)
-    {
-        await Clients.User(user).SendAsync("Notification", message);
-    }
-}
+public class ExpenseHub : Hub;
