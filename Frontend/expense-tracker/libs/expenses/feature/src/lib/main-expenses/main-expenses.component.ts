5463c3f63c41288b81928b432a5a8ed167341a4c
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-08-22 20:57:02 +0200

    Added Table

--- Frontend/expense-tracker/libs/expenses/feature/src/lib/main-expenses/main-expenses.component.ts
+++ Frontend/expense-tracker/libs/expenses/feature/src/lib/main-expenses/main-expenses.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ExpensesFormComponent } from '@expense-tracker/expenses/ui';
+import {
+  ExpensesFormComponent,
+  ExpensesListComponent,
+} from '@expense-tracker/expenses/ui';
 import { Expense } from '@expense-tracker/expenses/domain';
 
 @Component({
   selector: 'lib-main-expenses',
   standalone: true,
-  imports: [CommonModule, ExpensesFormComponent],
+  imports: [CommonModule, ExpensesFormComponent, ExpensesListComponent],
   templateUrl: './main-expenses.component.html',
   styleUrl: './main-expenses.component.scss',
 })
